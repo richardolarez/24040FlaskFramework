0b516995efbf036f694f8ee95b0114083cd42706
Richardo Larez <richardolarez@arizona.edu>
2024-04-25 15:14:39 -0700

    Uploading several projects work with a primary pair key

--- app/models/component.py
+++ app/models/component.py
@@ -3,10 +3,10 @@ from app import db
 class Component(db.Model):
     __tablename__ = "Component"
     componentId = db.Column(db.String(255), primary_key=True)
-    projectId = db.Column(db.Integer)
+    projectId = db.Column(db.Integer, primary_key=True)
     componentName = db.Column(db.String(255))
     partNumber = db.Column(db.String(255))
     componentType = db.Column(db.String(255))
 
     def json(self):
-        return{'componentId': self.componentId, 'projectId': self.projectId, 'componentName': self.componentName, 'partNumber': self.partNumber, 'componentType': self.componentType}
+        return {'componentId': self.componentId, 'projectId': self.projectId, 'componentName': self.componentName, 'partNumber': self.partNumber, 'componentType': self.componentType}
